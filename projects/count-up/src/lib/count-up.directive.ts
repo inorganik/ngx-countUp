7bae3a7a5927e301ef1ef79690a40c775b9721b4
Jamie Perkins <jamie.perkins@gmail.com>
2019-02-26 09:07:59 -0700

    update for 2.0 wip

--- projects/count-up/src/lib/count-up.directive.ts
+++ projects/count-up/src/lib/count-up.directive.ts
@@ -8,7 +8,7 @@ import {
   OnChanges,
   SimpleChanges
 } from '@angular/core';
-import * as CountUp from 'countup.js';
+import { CountUp, CountUpOptions } from 'countup.js';
 
 @Directive({
   selector: '[countUp]'
@@ -16,24 +16,11 @@ import * as CountUp from 'countup.js';
 export class CountUpDirective implements OnChanges {
 
   countUp: any;
-  // Optional extra configuration, such as easing.
-  @Input('countUp') options: any;
-
-  // Optional start value for the count. Defaults to zero.
-  @Input() startVal: number;
-
-  // the number to count to
-  @Input() endVal: number;
-
-  // Optional duration of the animation in seconds. Default is 2.
-  @Input() duration: number;
-
-  // Optional number of decimal places. Default is 2.
-  @Input() decimals: number;
-  
-  // Optional flag for specifying whether the element should re-animate when clicked.
-  // Default is true.
-  @Input() reanimateOnClick: boolean;
+  // the value you want to count to
+  @Input('countUp') endVal: number;
+  @Input() options: CountUpOptions = {};
+  @Input() reanimateOnClick = true;
+  @Output() complete = new EventEmitter<void>();
 
   // Re-animate if preference is set.
   @HostListener('click')
@@ -43,50 +30,19 @@ export class CountUpDirective implements OnChanges {
     }
   }
 
-  // on-complete event emitter
-  @Output() complete = new EventEmitter<void>();
-
   constructor(private el: ElementRef) {}
 
   ngOnChanges(changes: SimpleChanges) {
     if (changes.endVal && changes.endVal.currentValue) {
-      this.countUp = this.createCountUp();
+      this.countUp = new CountUp(this.el.nativeElement, this.endVal, this.options);
       this.animate();
     }
   }
 
-  private createCountUp(): any {
-    const start = this.startVal || 0;
-    const duration = this.duration || 2;
-    const decimals = this.decimals || 0;
-
-    if (!this.duration) {
-      this.duration = duration;
-    }
-
-    // construct countUp
-    let countUp = new CountUp(this.el.nativeElement, start, this.endVal, decimals, duration, this.options);
-    const diff = Math.abs(this.endVal - start);
-    // make easing smoother for large numbers
-    if (diff > 999) {
-      const up = (this.endVal > start) ? -1 : 1;
-      countUp = new CountUp(this.el.nativeElement, start, this.endVal + (up * 100), decimals, duration / 2, this.options);
-    }
-
-    return countUp;
-  }
-
   private animate() {
     this.countUp.reset();
-    if (this.endVal > 999) {
-      this.countUp.start(() => this.countUp.update(this.endVal));
-      setTimeout(() => {
-        this.complete.emit();
-      }, this.duration * 1000);
-    } else {
-      this.countUp.start(() => {
-        this.complete.emit();
-      });
-    }
+    this.countUp.start(() => {
+      this.complete.emit();
+    });
   }
 }
