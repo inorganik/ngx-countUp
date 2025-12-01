bd8e589f165314cfd49dc7a806a1d6f446e19256
inorganik <jamie.perkins@gmail.com>
2025-12-01 06:13:43 -0700

    prettier and tailwind setup

---
+++ .prettierrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+  tabWidth: 2,
+  useTabs: false,
+  singleQuote: true,
+  trailingComma: 'none',
+  printWidth: 100,
+  semi: true,
+  pluginSearchDirs: ['.'],
+};
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,17 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styles: `
+    .vertical-spacer {
+      height: 2500px;
+      width: 10px;
+      background: url(https://t3.ftcdn.net/jpg/03/41/31/88/360_F_341318810_iLrhXO8CD1mLSLIf7RkwXSahVuowqsgP.jpg)
+        1px 0;
+    }
+  `,
 })
 export class App {
   protected readonly title = signal('ngx-countUp');
